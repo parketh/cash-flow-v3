137209e3775d6d23de13e8ecf77f9f87e615ed3c
yeungparkhay <yeungparkhay@gmail.com>
2021-07-23 07:51:22 +0100

    added exportPathMap

--- next.config.js
+++ next.config.js
@@ -1,12 +1,29 @@
+const { CourseIndex } = require('./data/CourseIndex')
+
 module.exports = {
+    exportPathMap: async (defaultPathMap) => {
+        let pathMap = {
+            '/': { page: '/' },
+            '/config': { page: '/config'},
+            '/download': { page: '/download' },
+            '/course': { page: '/course'},
+        }
+
+        const coursePages = CourseIndex.shift()
+        coursePages.map(page => {
+            pathMap[`/course/${page}`] = { page: '/course', query: { slug: page } }
+        })
+
+        return pathMap
+    },    
     webpack: (config) => {
-      config.module.rules.push({
+        config.module.rules.push({
         test: /\.md$/,
         use: 'raw-loader',
-      })
-      return config
+        })
+        return config
     },
     eslint: {
-      ignoreDuringBuilds: true,
+        ignoreDuringBuilds: true,
     }
-  }
+}
