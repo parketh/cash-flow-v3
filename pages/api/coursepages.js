4028565d8db071d6a680ec6faa610db9336e1668
yeungparkhay <yeungparkhay@gmail.com>
2021-08-13 00:42:48 +0100

    Fixed route

--- pages/api/coursepages.js
+++ pages/api/coursepages.js
@@ -2,19 +2,18 @@ import { readFileSync } from "fs"
 import { join } from "path"
 
 const coursePagesHandler = (request, response) => {
-    if (request.method === "GET") {
-        const filepath = join(__dirname, "../") + `/content/pages/${request.params.slug}.md`
+    const {
+        query: { id },
+        method,
+    } = request
+    
+    if (method === "GET") {
+        const filepath = join(__dirname, "../") + `/content/pages/${id}.md`
         const file = readFileSync(filepath, "utf8")
         response.send(file)
     } else {
         response.status(405).end(`Method ${request.method} not allowed.`)
     }
 }
-
-coursePagesRouter.get("/:slug", (request, response) => {
-    const filepath = join(__dirname, "../") + `/content/pages/${request.params.slug}.md`
-    const file = readFileSync(filepath, "utf8")
-    response.send(file)
-})
 
 export default coursePagesHandler
