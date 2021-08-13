2022b91c799239a2c24fd53a2843caab823cc817
yeungparkhay <yeungparkhay@gmail.com>
2021-08-13 08:11:21 +0100

    updated absolute import path aliases

--- next.config.js
+++ next.config.js
@@ -6,20 +6,20 @@ module.exports = {
             test: /\.md$/,
             use: "raw-loader",
         })
-        config.resolve.alias = {
-            ...config.resolve.alias,
-            "@Elements": path.resolve(__dirname, "components/elements"),
-            "@Modules": path.resolve(__dirname, "components/modules"),
-            "@Sections": path.resolve(__dirname, "components/sections"),
-            "@Layouts": path.resolve(__dirname, "components/layouts"),
-            "@Utils": path.resolve(__dirname, "utils"),
-            "@FormOptions": path.resolve(__dirname, "data/form-options"),
-            "@FormValues": path.resolve(__dirname, "data/form-values"),
-            "@CourseIndex": path.resolve(__dirname, "data/course-index"),
-            "@Models": path.resolve(__dirname, "models"),
-            "@Services": path.resolve(__dirname, "services"),
-            "~": path.resolve(__dirname),
-        }
+        // config.resolve.alias = {
+        //     ...config.resolve.alias,
+        //     "@Elements": path.resolve(__dirname, "components/elements"),
+        //     "@Modules": path.resolve(__dirname, "components/modules"),
+        //     "@Sections": path.resolve(__dirname, "components/sections"),
+        //     "@Layouts": path.resolve(__dirname, "components/layouts"),
+        //     "@Utils": path.resolve(__dirname, "utils"),
+        //     "@FormOptions": path.resolve(__dirname, "data/form-options"),
+        //     "@FormValues": path.resolve(__dirname, "data/form-values"),
+        //     "@CourseIndex": path.resolve(__dirname, "data/course-index"),
+        //     "@Models": path.resolve(__dirname, "models"),
+        //     "@Services": path.resolve(__dirname, "services"),
+        //     "~": path.resolve(__dirname),
+        // }
         return config
     },
     eslint: {
