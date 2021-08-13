ff99dadfec1b7fee71928d24acaa9a2f2400b351
yeungparkhay <yeungparkhay@gmail.com>
2021-08-13 06:19:37 +0100

    fixed routes and filepaths

---
+++ pages/api/coursepages/[id].js
@@ -0,0 +1,19 @@
+import { readFileSync } from "fs"
+import { join } from "path"
+
+const coursePagesHandler = (request, response) => {
+    const {
+        query: { id },
+        method,
+    } = request
+
+    if (method === "GET") {
+        const filepath = join(__dirname, "../../../") + `/content/pages/${id}.md`
+        const file = readFileSync(filepath, "utf8")
+        response.send(file)
+    } else {
+        response.status(405).end(`Method ${request.method} not allowed.`)
+    }
+}
+
+export default coursePagesHandler
